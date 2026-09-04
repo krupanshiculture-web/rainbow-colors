2977309fd8620cf5364974615915aac826b0c495
Mohit Gohel <54216595+MohitGohel07@users.noreply.github.com>
2026-09-04 19:56:18 +0530

    Refactor routes to use HomeController for homepage and error handling

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+     public function index()
+    {
+        return view('frontend.home.index');
+    }
+
+    public function error()
+    {
+        return view('frontend.home.error');
+    }
+}
---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odor - Vape Store WooCommerce HTML Template</title>
+    <!-- Favicon img -->
+    <link rel="shortcut icon" href="{{ asset('frontend/assets/images/favicon.png') }}" type="image/x-icon">
+    <!-- Bootstarp min css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
+    <!-- All min css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/all.min.css') }}">
+    <!-- Swiper bundle min css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/swiper-bundle.min.css') }}">
+    <!-- Magnigic popup css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/magnific-popup.css') }}">
+    <!-- Animate css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/animate.css') }}">
+    <!-- Nice select css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/nice-select.css') }}">
+    <!-- Style css -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
+</head>
+
+<body>
+
+    {{-- Header --}}
+    @include('frontend.layouts.header')
+
+    <!-- Preloader area start -->
+    <div class="loading">
+        <span class="text-capitalize">L</span>
+        <span>o</span>
+        <span>a</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>g</span>
+    </div>
+
+    <div id="preloader">
+    </div>
+    <!-- Preloader area end -->
+
+    <!-- Mouse cursor area start here -->
+    <div class="mouse-cursor cursor-outer"></div>
+    <div class="mouse-cursor cursor-inner"></div>
+    <!-- Mouse cursor area end here -->
+
+  {{-- Page Content --}}
+    @yield('content')
+
+
+    {{-- Footer --}}
+    <footer>
+        @include('frontend.layouts.footer')
+    </footer>
+
+    <!-- Back to top area start here -->
+    <div class="scroll-up">
+        <svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!-- Back to top area end here -->
+
+    <!-- Jquery 3. 7. 1 Min Js -->
+    <script src=" {{ asset('frontend/assets/js/jquery-3.7.1.min.js') }}"></script>
+    <!-- Bootstrap min Js -->
+    <script src=" {{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
+    <!-- Swiper bundle min Js -->
+    <script src=" {{ asset('frontend/assets/js/swiper-bundle.min.js') }}"></script>
+    <!-- Counterup min Js -->
+    <script src=" {{ asset('frontend/assets/js/jquery.counterup.min.js') }}"></script>
+    <!-- Wow min Js -->
+    <script src=" {{ asset('frontend/assets/js/wow.min.js') }}"></script>
+    <!-- Magnific popup min Js -->
+    <script src=" {{ asset('frontend/assets/js/magnific-popup.min.js') }}"></script>
+    <!-- Nice select min Js -->
+    <script src=" {{ asset('frontend/assets/js/nice-select.min.js') }}"></script>
+    <!-- Pace min Js -->
+    <script src=" {{ asset('frontend/assets/js/pace.min.js') }}"></script>
+    <!-- Isotope pkgd min Js -->
+    <script src=" {{ asset('frontend/assets/js/isotope.pkgd.min.js') }}"></script>
+    <!-- Waypoints Js -->
+    <script src=" {{ asset('frontend/assets/js/jquery.waypoints.js') }}"></script>
+    <!-- Script Js -->
+    <script src=" {{ asset('frontend/assets/js/script.js') }}"></script>
+</body>
+
+</html>
